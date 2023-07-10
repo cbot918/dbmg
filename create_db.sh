4721927003bd7bcef3e06a16381c6a84a60ea728
cbot918 <cbot918@gmail.com>
2023-07-11 00:38:16 +0800

    fix_add_mysql_drive

--- create_db.sh
+++ create_db.sh
@@ -1,1 +1,21 @@
-docker run -p 5433:5432 --name dby -d -e POSTGRES_PASSWORD=12345 -e POSTGRES_DB=dby postgres
+NETWORK=qcode-server_default
+HOST=db
+DB_USER=root
+DB_PASSWORD=12345
+DB=qcode
+
+MY_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dby)
+
+# docker run -p 3307:3306 --name dby -d -e MYSQL_ROOT_PASSWORD=12345 -e POSTGRES_DB=dby postgres
+
+
+echo $MY_IP
+# docker run --name dby -e MYSQL_ROOT_PASSWORD=12345 -d mysql
+docker run -it --rm mysql mysql -h $MY_IP -u root -p$"12345" -e "create database dby"
+
+# docker run -it --rm mysql mysql -h <container_host> -P <container_port> -u <username> -p<password> <database_name>
+
+
+
+
+# docker run -it --rm --network $NETWORK mysql mysql -h db -u $DB_USER -p$"$DB_PASSWORD" $DB -e "$1"
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cbot918/dbmg
 go 1.19
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
